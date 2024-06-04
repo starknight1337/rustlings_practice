8176ceea465985920ae5f4e1d57468cb3f93e85d
Starknight1337 <the1337starknight@@gmail.com>
2024-06-04 22:32:38 +1000

    Fresh start - clean up broken git username and email settings

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -13,9 +13,8 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
+    println!("AWAWAWAWAWA!!");
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
     println!(r#"                 _   _ _                  "#);
